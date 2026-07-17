fb563a4046aa2ad87cc226051a9042e3e775de54
Rohan2731 <156290759+Rohan2731@users.noreply.github.com>
2026-07-17 14:30:27 +0530

    Update BackTestForm.jsx

--- src/components/BackTestForm.jsx
+++ src/components/BackTestForm.jsx
@@ -13,7 +13,7 @@ function BackTestForm({ setResults }) {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/backtest",
+        "https://trading-bot-frontend-dashboard.onrender.com/backtest",
         {
           method: "POST",
           headers: {
